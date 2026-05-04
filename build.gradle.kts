11f9595682eaf1265c8578af319b32687c9e391d
song-enzo <271486735+song-enzo@users.noreply.github.com>
2026-05-04 12:49:32 -0400

    Fix build configuration: update Gradle wrapper and fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,9 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("kotlin-kapt") version "1.9.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
